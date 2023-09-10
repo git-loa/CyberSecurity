387078e072abbee2f2cb4562834bae13338e20c0
Leonard Okyere Afeke <leo.afeke@outlook.com>
2023-09-09 23:43:38 -0400

    Modified get_network_info.py

--- python/execute_command/get_network_info.py
+++ python/execute_command/get_network_info.py
@@ -1,7 +1,62 @@
 #!/usr/bin/env python3
-import os, configparser
+import subprocess, os, configparser, re
 from collections import namedtuple
-#!/usr/bin/python
+
+
+def get_windows_saved_ssids():
+    """Returns a list of saved SSIDs in a Windows machine using netsh command"""
+    
+    command = "netsh wlan show profile"
+    network_profiles = subprocess.check_output(command.split(' '), shell=True).decode()
+    network_names = re.findall("(?:Profile\s*:\s*)(.*)", network_profiles)
+    return network_names
+
+def get_windows_saved_wifi_passorwds(verbose=1):
+    """Extracts saved Wi-Fi passwords saved in a Windows machine, this function extracts data using netsh
+    command in Windows
+    Args:
+        verbose (int, optional): whether to print saved profiles real-time. Defaults to 1.
+    Returns:
+        [list]: list of extracted profiles, a profile has the fields ["ssid", "ciphers", "key"]
+    """
+    
+    ssids = get_windows_saved_ssids()
+    Profile = namedtuple("Profile", ["ssid", "ciphers", "key"])
+    profiles = []
+    
+    for ssid in ssids:
+        ssid_details = subprocess.check_output(["netsh", "wlan", "show", "profile", ssid, "key=clear"], shell=True).decode()
+        
+        ciphers = re.findall("(?:Cipher\s*:\s*)(.*)", ssid_details)
+        ciphers = "/".join([c.strip() for c in ciphers])
+        
+        key = re.findall("(?:Key Content\s*:\s*)(.*)", ssid_details)
+        if len(key)!=0:
+            key = key[0].strip()
+        else:
+            key = None
+        
+        data = {"ssid":ssid.strip(), "ciphers":ciphers, "key":key}
+        
+        profile = Profile(**data)
+        profiles.append(profile)
+        
+        if verbose>=1:
+            print_windows_profile(profile)
+    return profiles
+    
+def print_windows_profile(profile):
+	"""Prints a single profile on Windows"""
+	print(f"{str(profile.ssid):26}{str(profile.ciphers):13}{str(profile.key):<20}")   
+	
+
+def print_windows_profiles(verbose):
+    """Prints all extracted SSIDs along with Key on Windows"""
+    print("SSID                     CIPHER(S)      KEY")
+    print("-"*50)
+    get_windows_saved_wifi_passorwds(verbose)
+
+
 
 def get_linux_saved_wifi_passwords(verbose=1):   
 	"""Extracts saved Wi-Fi passwords saved in a Linux machine, this function extracts data in the
@@ -43,7 +98,7 @@ def print_profiles(verbose = 1):
 	if os.name == "posix":
 		print_linux_profiles(verbose)
 	elif os.name == "nt":
-		print("Coming Soon")
+		print_windows_profiles(verbose)
 	else:
 		raise NotImplemented("Code only works for either LINUX of WINDOWS.")
 		
